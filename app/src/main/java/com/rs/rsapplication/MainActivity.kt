7493032df6bff993b2d73a3b1ffb97741218469e
mrutyunjay <mrutyunjay.senapati@processmap.com>
2023-05-14 16:12:51 +0530

    RegPage added

--- app/src/main/java/com/rs/rsapplication/MainActivity.kt
+++ app/src/main/java/com/rs/rsapplication/MainActivity.kt
@@ -8,24 +8,25 @@ import android.view.View.OnClickListener
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.rs.rsapplication.UI.AttendanceModuleFragment
+import com.rs.rsapplication.UI.RegistrationModuleFragment
 
-class MainActivity : AppCompatActivity(),OnClickListener {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     private var activeFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homescreen)
-       var text:TextView = findViewById(R.id.textGrid)
-        text.setOnClickListener {
-            showAttendanceModule()
-        }
-
+        val attendance:CardView = findViewById(R.id.attendance)
+        val reg:CardView = findViewById(R.id.reg)
+        val exam:CardView = findViewById(R.id.exam)
+      //  val report:CardView = findViewById(R.id.report)
+        attendance.setOnClickListener(this)
+        reg.setOnClickListener(this)
+        exam.setOnClickListener(this)
     }
-
-
-
 
     private fun showAttendanceModule() {
         if (activeFragment is AttendanceModuleFragment) return
@@ -50,14 +51,41 @@ class MainActivity : AppCompatActivity(),OnClickListener {
 
         activeFragment = fragment
     }
+    private fun showRegistrationModule() {
+        if (activeFragment is RegistrationModuleFragment) return
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        var fragment = supportFragmentManager.findFragmentByTag(RegistrationModuleFragment.TAG) as RegistrationModuleFragment?
+
+        if (fragment == null) {
+            fragment = RegistrationModuleFragment.newInstance()
+            fragmentTransaction.add(R.id.containerFragment, fragment, RegistrationModuleFragment.TAG)
+           val frameLayout:FrameLayout = findViewById(R.id.containerFragment)
+           val mainGridLayout:LinearLayout = findViewById(R.id.mainGrid)
+            frameLayout.visibility = View.VISIBLE
+            mainGridLayout.visibility = View.GONE
+        } else {
+            fragmentTransaction.show(fragment)
+        }
+
+        if (activeFragment != null) fragmentTransaction.hide(activeFragment as Fragment)
+
+        fragmentTransaction.commit()
+
+        activeFragment = fragment
+    }
     private fun showExamModule() {
        // Log.d(TAG,"ViewCreated")
     }
 
     override fun onClick(v: View?) {
-
-        when(v?.getId()){
-            R.id.attendance->showAttendanceModule()
+        when(v?.id){
+            R.id.attendance->{
+                showAttendanceModule()
+            }
+            R.id.reg->{
+                showRegistrationModule()
+            }
 
         }
     }
